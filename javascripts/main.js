f48a6dfb36d196b6a2c7e267d9001b1605ad3023
Michael Washburn <washburn.michael@gmail.com>
2015-12-01 15:11:57 -0800

    updated with shuffle function

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,15 +1,46 @@
 console.log("main js loaded.");
-$(document).ready(function(){
-
-var score = 
+$(document).ready(function(){ 
 
 $("#elevRollBtn").click(function(){
 	$("ul").append("<li>(a) Spring</li>", "<li>(b) Summer</li>", "<li>(c) Autumn</li>");
 	$("#text_display").append("<b>When is the best time to climb Mt. Everest?</b>");
-	$(function elevRoll(){
-
-	});
 });
+
+function shuffle (array) {
+  var i = 0
+    , j = 0
+    , temp = null
+
+  for (i = array.length - 1; i > 0; i -= 1) {
+    j = Math.floor(Math.random() * (i + 1))
+    temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+  }
+}
+
+var Card = function Card(question, correct, wrong1, wrong2){
+	this.question = question;
+	this.correctAnswer = correct;
+	this.possibleAnswers = [correct, wrong1, wrong2];
+
+	shuffle(this.possibleAnswers);
+	//set new shuffled order to positions
+	this.a = this.possibleAnswers[0];
+	this.b = this.possibleAnswers[1];
+	this.c = this.possibleAnswers[2];
+
+
+	this.checkIfRight = function(){
+		// write code here that compares this.correctAnswer with the answer a person chooses to see if the strings match
+		// return true if they match
+	}
+}
+
+var questionCollection = [
+	new Card("Question", "Correct Answer", "Wrong answer 1", "Wrong answer 2"),
+	new Card("Why did the chicken cross the road?", "To get to the other side", "Wrong answer 1", "Wrong answer 2")
+]
 
 
 var Player = function Player(){
@@ -44,7 +75,7 @@ var Game = function Game(){
 		return this.deck.pop()
 	}
 
-	this.elevRoll = function(){
+	this.elevRoll = function(min,max){
 		var playerElev = Math.round((Math.random() * (1500-500)) + 500);
 		document.getElementById("elevdisplay").innerHTML = playerElev; //create new <div> for display with new id
 		document.getElementById("elevRollBtn").disabled = true;
