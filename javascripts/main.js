cf46caa206960c3d21bbecf3ffd9c447b6a3a9d9
Michael Washburn <washburn.michael@gmail.com>
2015-12-01 14:15:15 -0800

    changes to js architecture code for game

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,27 +1,55 @@
 console.log("main js loaded.");
 $(document).ready(function(){
 
-
-//below is the first draft function for returning a random
-//attainable elevation for a player. Max elevation is 1500.
-//Min elevation is 500.
-
-// $("#elevdisplay").click(function elevRoll(min, max){
+var score = 
 
 $("#elevRollBtn").click(function(){
 	$("ul").append("<li>(a) Spring</li>", "<li>(b) Summer</li>", "<li>(c) Autumn</li>");
 	$("#text_display").append("<b>When is the best time to climb Mt. Everest?</b>");
-	$(function elevRoll(min,max){
-		var playerElev = Math.round((Math.random() * (1500-500)) + 500);
-		document.getElementById("elevdisplay").innerHTML = playerElev; //create new <div> for display with new id
+	$(function elevRoll(){
+
 	});
 });
 
-// $("#elevdisplay").click(function elevRoll(min, max){
-// var playerElev = Math.round((Math.random() * (1500-500)) + 500);
-// $("elevdisplay").html(playerElev);
-// });
 
+var Player = function Player(){
+	this.score = 0;
+	this.icon = "default icon name";
+}
+
+var Deck = function Deck(){
+	this.badCards = function(){
+		// takes a random 5 cards from all the bad card collection and return an array
+	}
+	this.questionCards = function(){
+		// takes a random 55 cards from all the question card collection and return an array
+	}
+	this.fullDeck = function(){
+		// takes the return value of the two functions above and shuffles them together into one nice 60 card deck returned as an array
+	}
+}
+
+var Game = function Game(){
+	this.player1 = new Player();
+	this.player2 = new Player();
+
+	this.scoreBoard = function(){
+        return "Player 1 is at " + this.player1.score + " elevation. And Player 2 is at " + this.player2.score + " elevation."
+    }
+
+	this.deck = new Deck();
+
+	this.draw = function(){
+		// write code that pulls the top card from the deck and shows the question on it
+		return this.deck.pop()
+	}
+
+	this.elevRoll = function(){
+		var playerElev = Math.round((Math.random() * (1500-500)) + 500);
+		document.getElementById("elevdisplay").innerHTML = playerElev; //create new <div> for display with new id
+		document.getElementById("elevRollBtn").disabled = true;
+	}
+}
 
 
 
