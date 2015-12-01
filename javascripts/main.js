81c7d773d5f5f31ea25e3ba2f472829cd8a81644
Michael Washburn <washburn.michael@gmail.com>
2015-11-30 16:59:20 -0800

    current html, js, and css inprogress

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,9 +4,7 @@ $(document).ready(function(){
 
 //below is the first draft function for returning a random
 //attainable elevation for a player. Max elevation is 1500.
-//Min elevation is 500. After testing, the random number
-//output exceeds 1500. May need to add an equality operator to
-//correct.
+//Min elevation is 500.
 
 $("#elevdisplay").click(function elevRoll(min, max){
   var playerElev = Math.round((Math.random() * (1500-500)) + 500);
