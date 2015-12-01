00fb22389beecae131bab32ce7bfa2e523a56436
Michael Washburn <washburn.michael@gmail.com>
2015-11-30 17:53:53 -0800

    removed some text in html to test jquery functions and appending

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,9 +6,15 @@ $(document).ready(function(){
 //attainable elevation for a player. Max elevation is 1500.
 //Min elevation is 500.
 
-$("#elevdisplay").click(function elevRoll(min, max){
-  var playerElev = Math.round((Math.random() * (1500-500)) + 500);
-    document.getElementById("elevdisplay").innerHTML = playerElev;
+// $("#elevdisplay").click(function elevRoll(min, max){
+
+//   var playerElev = Math.round((Math.random() * (1500-500)) + 500);
+//     document.getElementById("elevdisplay").innerHTML = playerElev; //change to jquery
+// });
+
+$("#elevRollBtn").click(function(){
+	$("Text_Display").append("When is the best time to climb Mt. Everest?");
+	$("ul").append("<li>'(a) Spring'</li>", "<li>'(b) Summer'</li>", "<li>'(c) Autumn'</li>");
 });
 
 
