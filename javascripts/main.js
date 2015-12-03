287d675d896b5d0800a8dc851e7e0e0f5736eab0
Michael Washburn <washburn.michael@gmail.com>
2015-12-02 19:19:14 -0800

    updated html and js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,14 @@
 console.log("main js loaded.");
 $(document).ready(function(){ 
 
+var player1Score = 0;
+var player2Score = 0;
+
+var score1 = document.getElementById("p1Score");
+score1.value = player1Score;
+
+
+
 // this code was taken from the internet
 function shuffle (array) {
   var i = 0
@@ -29,21 +37,24 @@ var Card = function Card(question, correct, wrong1, wrong2){
 	self.b = self.possibleAnswers[1];
 	self.c = self.possibleAnswers[2];
 
-
 	self.checkIfRight = function(chosenAnswer){
 		// write code here that compares self.correctAnswer with the answer a person chooses to see if the strings match
 		// return true if they match
 		console.log(chosenAnswer);
 		if (self.correctAnswer === chosenAnswer) {
-			console.log('Correct!')
-			return true
-		}
+			player1Score += 500;
+			p1Score.value = player1Score;
+		} else alert ("wrong");
+			counter++;
+		
 	}
 
 	self.printOut = function(){
 		$('#display_box').html('<div id="text_display1">' + self.question + '</div><div><ul id="choices_display"><li id="answerA" class="answer">' + self.a + '</li><li id="answerB" class="answer">' + self.b + '</li><li id="answerC" class="answer">' + self.c + '</li></ul></div>');
 	}
 }
+
+
 
 var Player = function Player(){
 	this.score = 0;
@@ -61,8 +72,9 @@ var Game = function Game(){
     }
 
 	this.deck = [
-		new Card("Question1", "correctAnswer", "wrong1", "Wrong answer 2"),
-		new Card("What is the best season to climb Mt. Everest?", "Spring", "Summer", "Autumn")
+		new Card("Question1", "correct", "wrong1", "Wrong answer 2"),
+		new Card("What is the best season to climb Mt. Everest?", "Spring", "Summer", "Autumn"),
+		new Card("At least how many deaths to date has Everest claimed?", "250", "400", "323")
 	]
 
 	this.draw = function(){
@@ -71,6 +83,11 @@ var Game = function Game(){
 
 		$('.answer').click(function(){
 			if (card.checkIfRight($(this)[0].innerHTML)){
+				//chosenAnswer === true
+
+
+
+
 				// move the character up in elevetation and start next turn
 				// add to player score
 			} else {
@@ -80,10 +97,11 @@ var Game = function Game(){
 	}
 
 }
-
+// count = 0
 game = new Game();
 
 $('#questionBtn').click(function(){
+	// count
 	game.draw();
 })
 
